2984d589580ef0987040d271f54d45912f752210
Valerie Bayard <valerie.bayard@gmail.com>
2022-09-26 19:01:33 +0100

    simplified PartOne implementation

--- src/main/java/PartOne.java
+++ src/main/java/PartOne.java
@@ -2,7 +2,7 @@ public class PartOne {
     public int findStopCodon(String dna, int startIndex, String stopCodon) {
         int currIndex = dna.indexOf(stopCodon,startIndex+3);
         while ( currIndex != -1 ) {
-            if ( (currIndex + 2 - startIndex + 1) % 3 == 0) {
+            if ( (currIndex - startIndex) % 3 == 0) {
                 return currIndex;
             }
             currIndex = dna.indexOf(stopCodon,currIndex+1);
