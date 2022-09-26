f448d63f70f7b407011c3860aa78b4ef8283398a
Valerie Bayard <valerie.bayard@gmail.com>
2022-09-26 19:35:11 +0100

    implemented findGene method in PartOne class and made tests pass

--- src/main/java/PartOne.java
+++ src/main/java/PartOne.java
@@ -1,4 +1,5 @@
 public class PartOne {
+
     public int findStopCodon(String dna, int startIndex, String stopCodon) {
         int currIndex = dna.indexOf(stopCodon,startIndex+3);
         while ( currIndex != -1 ) {
@@ -8,5 +9,26 @@ public class PartOne {
             currIndex = dna.indexOf(stopCodon,currIndex+1);
         }
         return dna.length();
+    }
+
+    public String findGene(String dna) {
+        int startIndex = dna.indexOf("ATG", 0);
+        int tagCodon = findStopCodon(dna, startIndex,"TAG");
+        int taaCodon = findStopCodon(dna, startIndex, "TAA");
+        int atgCodon = findStopCodon(dna, startIndex, "ATG");
+        int stopCodon = 0;
+
+        if (tagCodon < taaCodon) {
+            stopCodon = tagCodon;
+        } else {
+            stopCodon = taaCodon;
+        }
+        if (stopCodon > atgCodon) {
+            stopCodon = atgCodon;
+        }
+        if (stopCodon < dna.length()) {
+            return dna.substring(startIndex,stopCodon + 3);
+        }
+        return "";
     }
 }
